cc5f06446ea0e7f827d1ee9b61f0d156f0495581
tdncoder <92639411+tdncoder@users.noreply.github.com>
2024-01-07 15:59:05 +0700

    Nguyen Update code va sua lai vai cho

--- easy-learn-web/easyLearnWeb/src/main/java/com/easy/learn/web/controller/LessonController.java
+++ easy-learn-web/easyLearnWeb/src/main/java/com/easy/learn/web/controller/LessonController.java
@@ -1,8 +1,10 @@
 package com.easy.learn.web.controller;
 
 
+import com.easy.learn.web.callApi.CourseCallService;
 import com.easy.learn.web.callApi.LessonCallService;
 import com.easy.learn.web.consts.UrlPath;
+import com.easy.learn.web.dto.course.Course;
 import com.easy.learn.web.dto.lesson.Lesson;
 import com.easy.learn.web.dto.trainer.Trainer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,17 +20,24 @@ import java.util.List;
 public class LessonController {
     @Autowired
     LessonCallService service;
+    @Autowired
+    CourseCallService courseCallService;
 
-    @GetMapping(UrlPath.GET_LESSON_BY_COURSEID + "/{id}")
-    public String showLessonPage(@PathVariable("id") Long id, Model model, RedirectAttributes ra) {
+    @GetMapping(UrlPath.GET_LESSON_BY_COURSE_ID)
+    public String showLessonPage(@PathVariable Long id, Model model, RedirectAttributes ra) {
         try {
             List<Lesson> lessonList = service.getAllLessonOfCourseID(id);
+            Course course = courseCallService.getCourseById(id);
+            if(course.getId() == null) {
+                System.out.println("Course not Found!");
+            }
+            model.addAttribute("detailsCourse", course);
             model.addAttribute("lessonList", lessonList);
             model.addAttribute("#", "Edit Trainer (ID: "+ id +")");
-            return "lesson";
+            return "student-take-course";
         } catch (Exception e) {
             ra.addFlashAttribute("message", "Course lessons not found");
-            return "redirect:/course";
+            return "courses";
         }
     }
 }
--- easy-learn-web/easyLearnWeb/src/main/java/com/easy/learn/web/dto/lesson/LessonDTO.java
+++ easy-learn-web/easyLearnWeb/src/main/java/com/easy/learn/web/dto/lesson/LessonDTO.java
@@ -1,7 +1,17 @@
 package com.easy.learn.web.dto.lesson;
 
 import com.easy.learn.web.dto.ReponseDTO;
-import com.easy.learn.web.dto.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LessonDTO extends ReponseDTO<Lesson> {
+    private Long id;
+    private String title;
+    private String duration;
+    private String url;
+    private Long courseId;
 }
