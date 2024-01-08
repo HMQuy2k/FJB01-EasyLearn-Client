9b7ca37786d3ab1691166b5c43ae59a618d64c52
Your Name <135402390+HMQuy2k@users.noreply.github.com>
2024-01-08 19:29:52 +0700

    Nguyen Upload Code
    (cherry picked from commit 03a2406998dc27ace265d21b725a09d2db95e2df)

--- easy-learn-web/easyLearnWeb/src/main/java/com/easy/learn/web/consts/UrlPath.java
+++ easy-learn-web/easyLearnWeb/src/main/java/com/easy/learn/web/consts/UrlPath.java
@@ -6,7 +6,9 @@ public interface UrlPath {
     String STUDENT_GET_ALL_COURSE = "/student-course";
     String STUDENT_COURSE_GET_ID = "/student-course/{id}";
     String PAYMENT = "/student-course/payment/{id}";
-    String GET_LESSON_BY_COURSE_ID = "/student-course/lesson/{id}";
+
+String GET_COURSE_BY_ID = "/course";
+String GET_LESSON_BY_COURSEID = "/course/lesson";
 
 
 }
